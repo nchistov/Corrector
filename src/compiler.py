179d786f94e92054b12e5dd9c222d5d2dc6d9221
nchistov <nikola-chistov@mail.ru>
2025-04-15 09:18:37 +0300

    change parser, write compiler's draft

---
+++ src/compiler.py
@@ -0,0 +1,80 @@
+from enum import Enum
+
+from parser import Parser
+
+class WaitingFor(Enum):
+    Procedure = 0
+    ProcedureName = 1
+    Command = 2
+    Symbol = 3
+    Check = 4
+    Number = 5
+
+
+class Compiler:
+    def __init__(self):
+        self.bytecode = bytearray()
+        self.state = WaitingFor.Procedure
+        self.indent = 0
+
+        self.procedures = {}
+        self.commands = {'ВПРАВО': 0x05,
+                         'ВЛЕВО': 0x06,
+                         'ПИШИ': 0x07,
+                         'ЯЩИК+': 0x08,
+                         'ЯЩИК-': 0x09,
+                         'ОБМЕН': 0x0A,
+                         'ПЛЮС': 0x0B,
+                         'МИНУС': 0x0C,
+                         'СТОЯТЬ': 0x0D
+        }
+        self.parser = Parser()
+
+    def compile(self, code) -> bytearray:
+        for tok in self.parser.parse(code):
+            match self.state:
+                case WaitingFor.Procedure:
+                    if tok.type == 'KEYWORD' and tok.value == 'ЭТО':
+                        self.state = WaitingFor.ProcedureName
+                    else:
+                        ...
+                case WaitingFor.ProcedureName:
+                    if tok.type == 'WORD':
+                        self.handle_procedure(tok.value)
+                case WaitingFor.Command:
+                    if tok.type == 'KEYWORD':
+                        match tok.value:
+                            case 'КОНЕЦ':
+                                self.handle_end()
+                            case _:
+                                ...
+                    elif tok.type == "COMMAND":
+                        self.handle_command(tok.value)
+                case WaitingFor.Number:
+                    ...
+
+        return self.bytecode
+
+    def handle_procedure(self, name):
+        self.procedures[name] = len(self.procedures)
+        self.bytecode.append(0x00)
+        self.bytecode.append(self.procedures[name])
+        self.state = WaitingFor.Command
+        self.indent += 1
+
+    def handle_end(self):
+        self.indent -= 1
+        self.bytecode.append(0x01)
+
+        if self.indent == 0:
+            self.state = WaitingFor.Procedure
+        else:
+            self.state = WaitingFor.Command
+
+    def handle_command(self, name):
+        self.bytecode.append(self.commands[name])
+        if name == 'ПИШИ':
+            self.state = WaitingFor.Number
+
+c = Compiler()
+print(c.compile("ЭТО Программа ВПРАВО КОНЕЦ"))
--- src/parser.py
+++ src/parser.py
@@ -2,42 +2,51 @@ from typing import NamedTuple
 
 class Token(NamedTuple):
     type:   str
-    value:  str
+    value:  str | int
     line:   int
     row: int
 
 
-def parse(code):
-    keywords = ['ЭТО', 'ЕСЛИ', 'ПОКА', 'КОНЕЦ', 'ПОВТОРИ', 'ТО', 'НЕ']
-    commands = ['ВПРАВО', 'ВЛЕВО', 'ПИШИ', 'ЯЩИК+', 'ЯЩИК-', 'ОБМЕН', 'ПЛЮС', 'МИНУС', 'СТОЯТЬ']
-    symbols = ['ПУСТО', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-               'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З','И', 'Й', 'К',
-               'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
-               'Ц', 'Я', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'ПРОБЕЛ'
-               '-', '+', '/', '*', '=', '<', '>', '(', ')', '[', ']', '{', '}', '.',
-               ',', '!', '?', ';', ':', '\'', '"', '#', '|', '$', '%', '~', '@']
-    checks = {'Я=Л', 'Я>Л', 'Я<Л', 'Я#Л'}
+class Parser:
+    def __init__(self):
+        self.keywords = ['ЭТО', 'ЕСЛИ', 'ПОКА', 'КОНЕЦ', 'ПОВТОРИ', 'ТО', 'НЕ']
+        self.commands = ['ВПРАВО', 'ВЛЕВО', 'ПИШИ', 'ЯЩИК+', 'ЯЩИК-', 'ОБМЕН', 'ПЛЮС', 'МИНУС', 'СТОЯТЬ']
+        self.symbols = ['ПУСТО', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                   'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З','И', 'Й', 'К',
+                   'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
+                   'Ц', 'Я', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'ПРОБЕЛ'
+                                                                     '-', '+', '/', '*', '=', '<', '>', '(', ')', '[', ']', '{', '}', '.',
+                   ',', '!', '?', ';', ':', '\'', '"', '#', '|', '$', '%', '~', '@']
+        self.checks = ['Я=Л', 'Я>Л', 'Я<Л', 'Я#Л']
 
-    line = row = 1
-    cur_token = ''
+        self.line = self.row = 1
+        self.cur_token = ''
 
-    for symbol in code.upper():
-        if symbol.isspace():
-            if cur_token:
-                if cur_token in symbols:
-                    yield Token('SYMBOL', cur_token, line, row)
-                elif cur_token in keywords:
-                    yield Token('KEYWORD', cur_token, line, row)
-                elif cur_token in commands:
-                    yield Token('COMMAND', cur_token, line, row)
-                elif cur_token in checks:
-                    yield Token('CHECK', cur_token, line, row)
-                else:
-                    yield Token('WORD', cur_token, line, row)
-                cur_token = ''
-            if symbol == '\n':
-                row = 0
-                line += 1
+    def parse(self, code):
+        for symbol in code.upper():
+            if symbol.isspace():
+                if self.cur_token:
+                    yield self._get_tok()
+                    self.cur_token = ''
+                if symbol == '\n':
+                    self.row = 0
+                    self.line += 1
+            else:
+                self.cur_token += symbol
+            self.row += 1
+        if self.cur_token:
+            yield self._get_tok()
+
+    def _get_tok(self) -> Token:
+        if self.cur_token in self.symbols:
+            return Token('SYMBOL', self.cur_token, self.line, self.row)
+        elif self.cur_token in self.keywords:
+            return Token('KEYWORD', self.cur_token, self.line, self.row)
+        elif self.cur_token in self.commands:
+            return Token('COMMAND', self.cur_token, self.line, self.row)
+        elif self.cur_token in self.checks:
+            return Token('CHECK', self.cur_token, self.line, self.row)
+        elif self.cur_token.isdigit():
+            return Token('NUMBER', int(self.cur_token), self.line, self.row)
         else:
-            cur_token += symbol
-        row += 1
+            return Token('WORD', self.cur_token, self.line, self.row)
